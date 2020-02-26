882126e3800aad891f18921858ecfd954a6e4bbb
David Parkinson <davidparkinson@kasi.re.kr>
2020-02-26 11:26:40 +0900

    Added first draft of testing code

---
+++ vocab_test.py
@@ -0,0 +1,37 @@
+#Quick script to test me on my Korean vocab
+from numpy import random
+import numpy as np
+if __name__ == "__main__":
+    #open vocab file
+    f = open('vocab.txt','r')
+    lines = f.readlines()
+    english = []
+    korean = []
+    for line in lines:
+        c = line.split()
+        english.append(c[0])
+        korean.append(c[1])
+
+    f.close()
+    n = len(english)
+    length_test = 2
+    k = random.randint(0,n,size=length_test)
+    success = 0
+    failure = 0
+    for ik in k:
+        print("What is the Korean word for {0:}?".format(english[ik]))
+        guess = input()
+        print("You wrote {0:}".format(guess))
+        print("The answer is {0:}".format(korean[ik]))
+        if guess == korean[ik]:
+            success +=1
+            print("Correct!")
+        else:
+            failure +=1
+            print("Incorrect")
+
+    print("You got {0:} out of {1:} correct".format(success,length_test))
+    if np.float32(success)/np.float32(length_test) > 0.6:
+        print("Well done")
+    else:
+        print("Try again")
